180ecfbfc2333dc5d0314561bf1fab976e1b90bd
croumegous <cyril.roumegous@gmail.com>
2023-04-10 11:58:20 +0200

    add zero shot classification based on intent name

--- nlu/app.py
+++ nlu/app.py
@@ -8,6 +8,9 @@ from fastapi import FastAPI
 
 from nlu.apis.v1 import api_router
 
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 logger = logging.getLogger(__name__)
 app = FastAPI()
 
@@ -15,9 +18,4 @@ app.include_router(api_router, prefix="/v1")
 
 
 if __name__ == "__main__":
-    load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    if openai.api_key is None:
-        raise ValueError("OPENAI_API_KEY not found in environment variables")
-
     uvicorn.run(app, host="0.0.0.0", port=8082)
