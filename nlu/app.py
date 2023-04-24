4410db82b59eea6d4eef475f2106a1bbc2c354f5
Julian Labatut <julian.labatut@etu.umontpellier.fr>
2023-04-24 13:54:38 +0200

    ci: upload Docker image to GHCR on Git tag Co-authored-by: croumegous <cyril.roumegous@gmail.com>

--- nlu/app.py
+++ nlu/app.py
@@ -1,18 +1,38 @@
 import logging
 import os
+import sys
 
 import openai
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from nlu.apis.v1 import api_router
+from nlu.database.client import Database
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 logger = logging.getLogger(__name__)
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    try:
+        await Database()
+    except Exception as e:
+        logger.error(f"Error connecting to database: {e}")
+        sys.exit(1)
+
+    if os.getenv("FUNCTIONS_GATEWAY") is None:
+        logger.error("FUNCTIONS_GATEWAY is not set")
+        sys.exit(1)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router, prefix="/v1")
 
